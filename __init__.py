70403cbc27597aafc442ffc620d8e88247b8da20
doakey3 <doakey3@gmail.com>
2019-02-22 19:48:07 -0600

    version update

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ bl_info = {
     "name": "Grease Writer",
     "description": "Automatic hand-drawn text animation",
     "author": "doakey3",
-    "version": (0, 0, 3),
+    "version": (0, 0, 4),
     "blender": (2, 80, 0),
     "wiki_url": "https://github.com/doakey3/GreaseDraw",
     "tracker_url": "https://github.com/doakey3/GreaseDraw/issues",
